e02a81ed310504e6ceea756bf4bb4adb2a9c5676
DeaSTL <jhartway99@gmail.com>
2019-11-09 23:44:50 -0500

    Added forge project

---
+++ src/main/java/com/stryphic/industrialcapitalism/util/Reference.java
@@ -0,0 +1,7 @@
+package com.stryphic.industrialcapitalism.util;
+
+public class Reference {
+    public static final String MODID = "examplemod";
+    public static final String NAME = "Industrial Capitalism";
+    public static final String VERSION = "0.0.1";
+}
