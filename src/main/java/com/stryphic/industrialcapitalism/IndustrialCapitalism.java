e686c0d7cd8cb30f84782de5e3b772c9c28a070b
DeaSTL <jhartway99@gmail.com>
2019-11-12 03:00:39 -0500

    Added Stryphic Core

--- src/main/java/com/stryphic/industrialcapitalism/IndustrialCapitalism.java
+++ src/main/java/com/stryphic/industrialcapitalism/IndustrialCapitalism.java
@@ -1,6 +1,8 @@
 package com.stryphic.industrialcapitalism;
 
 import com.stryphic.industrialcapitalism.util.Reference;
+import com.stryphic.stryphiccore.blocks.BlockGreen;
+import com.stryphic.stryphiccore.entityproperties.CorePlayerCapability;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -14,6 +16,7 @@ public class IndustrialCapitalism
 
     public static Logger logger;
 
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
@@ -24,6 +27,7 @@ public class IndustrialCapitalism
     public void init(FMLInitializationEvent event)
     {
         // some example code
+
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
